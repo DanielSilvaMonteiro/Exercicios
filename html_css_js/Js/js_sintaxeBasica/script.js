5ef38d3ca6eeb486ff1294fb82b5e905965a4e74
Daniel Silva Monteiro <89706085+DanielSilvaMonteiro@users.noreply.github.com>
2022-05-24 13:29:26 -0300

    Add exercise

---
+++ html_css_js/Js/js_sintaxeBasica/script.js
@@ -0,0 +1,15 @@
+alert ("Olá seja bem vindo!");
+
+//var geralemente é utilizado em global
+var number = 10;
+console.log(typeof(number));
+console.log(number);
+
+function conta(){
+//let geralmente é utilizado em local
+let soma = 10+ number;
+console.log(soma);
+alert("conta realizada");
+}
+conta();
+
