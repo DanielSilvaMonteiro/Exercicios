2ac41385474068a49018529ccefbf6b42a5cf0ba
Daniel Silva Monteiro <89706085+DanielSilvaMonteiro@users.noreply.github.com>
2022-06-02 13:32:44 -0300

    add exercise about function

---
+++ html_css_js/Js/Aprofundando Funcoes/script.js
@@ -0,0 +1,21 @@
+//função declarativa
+function funcao(){
+    console.log("esta é uma função declarativa")
+}
+
+funcao();
+
+
+//função de expressão
+var funcao = function(){
+    console.log("Sou uma função de expressão");
+}
+
+funcao();
+
+//arrow function- sempre anonimas
+var funcaoArrow = () =>{
+    console.log("Sou uma função arrow function");
+}
+
+funcaoArrow();
