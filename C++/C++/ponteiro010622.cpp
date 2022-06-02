d7d77048bffd9758b5bc6923c02473267138d3be
Daniel Silva Monteiro <89706085+DanielSilvaMonteiro@users.noreply.github.com>
2022-06-01 21:34:42 -0300

    add exercise

---
+++ C++/C++/ponteiro010622.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <math.h> 
+#include <iostream> 
+
+using namespace std; 
+int main() {
+const int VAL= 124;
+int *p;
+*p = VAL;
+
+cout << *p;
+}
