d0945fefcbc0d3dcb24db4c5225be7d8713eae93
Daniel Silva Monteiro <89706085+DanielSilvaMonteiro@users.noreply.github.com>
2022-06-02 13:16:36 -0300

    add exercise Js

---
+++ html_css_js/Js/Funcoes e Particularidades/script.js
@@ -0,0 +1,15 @@
+
+//Função declaração
+function funcao(){
+    console.log("Tudo certo jovem!");
+}
+
+funcao();
+
+//Funções com parametros
+
+function mensagem(primeiro, segundo){
+console.log(primeiro, segundo);
+}
+
+mensagem("tudo certo", "função ok");
