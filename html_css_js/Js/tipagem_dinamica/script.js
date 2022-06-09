4e40521655ae266c21eb164022773156bd683363
Daniel Silva Monteiro <89706085+DanielSilvaMonteiro@users.noreply.github.com>
2022-06-09 13:15:54 -0300

    add js exercise

---
+++ html_css_js/Js/tipagem_dinamica/script.js
@@ -0,0 +1,11 @@
+/** exemplo de tipagem dinamica, as variaveis podem receber outro tipo de informeção
+exemplo string virar number
+*/
+
+let um  = "um";
+console.log(um);
+typeof um;
+
+um = 1;
+console.log(um);
+typeof um;
