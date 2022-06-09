3fbd3dcd2669db5a45492942132fea821e54c0b4
Daniel Silva Monteiro <89706085+DanielSilvaMonteiro@users.noreply.github.com>
2022-06-09 13:02:52 -0300

    add exercise js

---
+++ html_css_js/Js/Constantes/script.js
@@ -0,0 +1,10 @@
+
+/*A constante precisa ser iniciada, não aceita valor de 
+atribuição depois de iniciada, devido a ser uma tentativa
+de reatribuição.*/
+
+const FIRST_NAME = "Daniel";
+
+console.log(FIRST_NAME);
+
+
