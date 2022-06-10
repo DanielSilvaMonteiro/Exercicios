ddeb360e3f1b6a720a31a819fb4e709ee833c197
Daniel Silva Monteiro <89706085+DanielSilvaMonteiro@users.noreply.github.com>
2022-06-10 13:22:58 -0300

    add js exercise - numbers

---
+++ html_css_js/Js/numbers/script.js
@@ -0,0 +1,28 @@
+let num = 100;
+
+let valor = num+5
+console.log(valor);
+
+valor = num / 5;
+console.log(valor);
+
+valor = num % 2;
+console.log(valor);
+
+//Math é tipo objeto
+typeof Math;
+
+//Math + PI
+console.log(Math.PI);
+
+let fiveBythree = 5/3;
+console.log(fiveBythree);
+
+//arredondamento para baixo
+Math.floor(fiveBythree);
+
+//arredondamento para cima
+Math.ceil(fiveBythree)
+
+fiveBythree.toString();
+typeof fiveBythree;
