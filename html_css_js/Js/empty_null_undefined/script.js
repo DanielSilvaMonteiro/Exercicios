6a465d024dce0d6e888e6723e531260c0a526f25
Daniel Silva Monteiro <monteiro.dan@outlook.com>
2022-06-12 17:55:53 -0300

    add js exercise

---
+++ html_css_js/Js/empty_null_undefined/script.js
@@ -0,0 +1,29 @@
+//undefined
+console.log(typeof abacaxi);
+
+let maca;
+console.log(typeof maca);
+
+//empty value
+let pera = "";
+console.log(typeof pera);
+console.log(pera);
+
+//nulo
+pera = null;
+console.log(typeof pera);
+console.log(pera);
+
+console.log(pera === false);
+console.log(pera === true);
+
+let fruta = pera === false;
+console.log(fruta);
+
+//falsy value
+console.log(!pera);
+
+let banana;
+console.log(!banana);
+
+console.log(undefined === false);
