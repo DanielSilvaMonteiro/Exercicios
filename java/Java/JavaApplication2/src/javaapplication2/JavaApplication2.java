7c634e06834bc1a1d9c31bae52d1f65e512bebba
Daniel Silva Monteiro <89706085+DanielSilvaMonteiro@users.noreply.github.com>
2022-08-16 21:07:49 -0300

    first commit

---
+++ java/Java/JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author lab7aluno
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JOptionPane.showMessageDialog(null, "\n Bem-vindo\n à programação Java \n\t\t por Janelas");
+        System.exit(0);
+    }
+    
+}
